8213f69588db216909463acaf830889a867b7d85
bikashghimire <bikashghimire2610@gmail.com>
2022-09-04 18:05:13 +0300

    Added json to represent data and made components dynamic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Main from "./components/Main.js";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
